100e5c7f47fc1a87d6619c58fe85ec2e8067e545
Luis Valverde <luvalfer2009@hotmail.com>
2022-07-16 18:18:13 -0300

    añadiendo glicerol

--- Molaridad.py
+++ Molaridad.py
@@ -1,8 +1,10 @@
 #para reactivos que tengan peso y sean en polvo
+NACL= 58.44
+HEPES=238.30
+MGCL2ANHIDRO=95.21
+MGCL2HEXAHIDRATO=203,31 
 MolecularWeight= 58.44	
 VolumenLitros= 0.25
 MolaridadMoles= 0.3
 Pesoengramos= MolecularWeight*VolumenLitros*MolaridadMoles
 print(f'{"Tienes que pesar "}{Pesoengramos}{" "}{"gramos"}{" "}{"en"}{" "}{VolumenLitros}{"Litros"}{" "}{"para tenerlo a"}{" "}{MolaridadMoles}{"Moles"}')
-
-#Para glicerol
