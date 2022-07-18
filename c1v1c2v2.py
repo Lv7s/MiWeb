d7c3686a36e963a4bc1ac294a7b413a1f628dfd4
Luis Valverde <luvalfer2009@hotmail.com>
2022-07-18 08:59:33 -0300

    c1v1c2v2 nuevo

--- c1v1c2v2.py
+++ c1v1c2v2.py
@@ -1,7 +1,25 @@
-conc_inicial = 291.2
-vol_inicial = 5
-#conc_final = 
-vol_final = 50
-
-resp_conc_final = conc_inicial*vol_inicial/vol_final
-print(f'{"Tienes: "}{vol_final}{"uL"}{" "}{"a una concentracion de"}{" "}{resp_conc_final}{"ng/uL"}')
+CalcDeseado = (input("¿Que deseas calcular?: "))
+if CalcDeseado == "c1":
+    v1 = int(input("Introduce el volumen 1: "))
+    c2 = int(input("Introduce la concentración 2: "))
+    v2 = int(input("Introduce el volumen 2: "))
+    c1 = (c2*v2)/v1
+    print(f'{"tu concentracion 1 es "}{c1}')
+if CalcDeseado == "v1":
+    c1 = int(input("Introduce la concentración 1: "))
+    c2 = int(input("Introduce la concentración 2: "))
+    v2 = int(input("Introduce el volumen 2: "))
+    v1 = (c2*v2)/c1
+    print(f'{"tu volumen 1 es "}{v1}')
+if CalcDeseado == "c2":
+    c1 = int(input("Introduce la concentración 1: "))
+    v1 = int(input("Introduce el volumen 1: "))
+    v2 = int(input("Introduce el volumen 2: "))
+    c2 = (c1*v1)/v2
+    print(f'{"tu concentracion 2 es "}{c2}')
+if CalcDeseado == "v2":
+    c1 = int(input("Introduce la concentración 1: "))
+    v1 = int(input("Introduce el volumen 1: "))
+    c2 = int(input("Introduce la concentración 2: "))
+    v2 = (c1*v1)/c2
+    print(f'{"tu volumen 2 es "}{v2}')
